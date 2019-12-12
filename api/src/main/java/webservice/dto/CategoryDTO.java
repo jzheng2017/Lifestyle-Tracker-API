270b54d030d530ecc7714f73f69954bc06751bda
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-12 18:35:40 +0100

    removed dates from dto

--- api/src/main/java/webservice/dto/CategoryDTO.java
+++ api/src/main/java/webservice/dto/CategoryDTO.java
@@ -1,24 +1,19 @@
 package webservice.dto;
 
 import javax.validation.constraints.NotEmpty;
-import java.time.LocalDateTime;
 
 public class CategoryDTO {
     private Integer id;
     @NotEmpty(message = "Category name can not be empty")
     private String name;
     private Integer parentId;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
     public CategoryDTO(){}
 
-    public CategoryDTO(Integer id, String name, Integer parentId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public CategoryDTO(Integer id, String name, Integer parentId) {
         this.id = id;
         this.name = name;
         this.parentId = parentId;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
     public CategoryDTO(int categoryId, String categoryName, Integer parentId) {
@@ -51,19 +46,4 @@ public class CategoryDTO {
         this.id = id;
     }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
