34ea92e7b2a453ca821d704953a1d1fc68fd6baf
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-17 21:45:42 +0100

    add: jwt authentication and authorization filter

--- api/src/main/java/webservice/configs/SecurityConfig.java
+++ api/src/main/java/webservice/configs/SecurityConfig.java
@@ -1,13 +1,69 @@
 package webservice.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import webservice.security.UserDetailsServiceImpl;
+import webservice.security.filters.AuthenticationFilter;
+import webservice.security.filters.AuthorizationFilter;
+import webservice.util.JwtUtil;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private UserDetailsServiceImpl userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void setbCryptPasswordEncoder(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    public void setJwtUtil(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+//    @Bean
+//    public UnauthorizedHandler unauthorizedHandler() {
+//        return new UnauthorizedHandler();
+//    }
+//
+//    @Bean
+//    public ForbiddenHandler forbiddenHandler() {
+//        return new ForbiddenHandler();
+//    }
+//
+//    @Bean
+//    public AuthenticationFilter authenticationFilter() {
+//        return new AuthenticationFilter();
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users/add").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager(), jwtUtil))
+                .addFilter(new AuthorizationFilter(authenticationManager(), jwtUtil))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
 }
