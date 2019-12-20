6bb7e038c3c8c23f4cc62da3fd5ff0d373a01737
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-20 18:51:14 +0100

    add: unit test, 100% code coverage

--- src/test/webservice/services/KeyServiceTest.java
+++ src/test/webservice/services/KeyServiceTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.Assertions;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import webservice.configs.AppConfig;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class KeyServiceTest {
     @InjectMocks
+    @Spy
     private KeyService keyService;
     @Mock
     private AppConfig appConfig;
@@ -26,11 +32,45 @@ public class KeyServiceTest {
     public void ConvertSecretKeyToStringReturnsString() {
         final byte[] bytes = {4, 2, 0};
         final Key secretKey = new SecretKeySpec(bytes, "HmacSHA256");
+
         Assertions.assertNotNull(keyService.ConvertSecretKeyToString(secretKey));
     }
 
     @Test
     public void ConvertStringToSecretKeyReturnsSecretKey() {
+        final String secretKey = "verySecretKey";
 
+        Assertions.assertNotNull(keyService.ConvertStringToSecretKey(secretKey));
+    }
+
+    @Test
+    public void getSecretKeyReturnsSecretKey(){
+        final String secretKey = "verySecretKey";
+
+        when(appConfig.getSecret()).thenReturn(secretKey);
+
+        Assertions.assertNotNull(keyService.getSecretKey());
+    }
+
+    @Test
+    public void getSecretKeyCallsAppConfigGetSecretKey(){
+        final String secretKey = "verySecretKey";
+
+        when(appConfig.getSecret()).thenReturn(secretKey);
+
+        keyService.getSecretKey();
+
+        verify(appConfig).getSecret();
+    }
+
+    @Test
+    public void getSecretKeyCallsConvertStringToSecretKeyFunction(){
+        final String secretKey = "verySecretKey";
+
+        when(appConfig.getSecret()).thenReturn(secretKey);
+
+        keyService.getSecretKey();
+
+        verify(keyService).ConvertStringToSecretKey(anyString());
     }
 }
