77e3627989529a2554241460dd850d78257e5842
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-28 15:19:49 +0100

    add: javadoc to user controller

--- src/main/java/webservice/controllers/UserController.java
+++ src/main/java/webservice/controllers/UserController.java
@@ -30,49 +30,102 @@ public class UserController {
         this.transactionService = transactionService;
     }
 
-    // USER CRUDS
+    /**
+     * Get all users
+     *
+     * @return ResponseEntity object containing a list of users
+     */
     @GetMapping("/all")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    /**
+     * Get a single user
+     *
+     * @param userId the id of a user
+     * @return ResponseEntity object containing a user
+     */
     @GetMapping("{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable("id") int userId) {
         return ResponseEntity.ok(userService.getUser(userId));
     }
 
+    /**
+     * Delete a single user
+     *
+     * @param userId the id of a user
+     * @return a boolean value, true if user has been deleted, otherwise false
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> deleteUser(@PathVariable("id") int userId) {
         return ResponseEntity.ok(userService.deleteUser(userId));
     }
 
+    /**
+     * Update a user
+     *
+     * @param user object containing the new values
+     * @return user object containing the updated values
+     */
     @PutMapping("update")
     public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UserDTO user) {
         return ResponseEntity.ok(userService.updateUser(user));
     }
 
+    /**
+     * Add a single user
+     *
+     * @param user object containing the registration values
+     * @return registered user
+     */
     @PostMapping("/add")
     public ResponseEntity<UserDTO> addUser(@Valid @RequestBody RegistrationDTO user) {
         return ResponseEntity.ok(userService.addUser(user));
     }
 
-    //USER TRANSACTION CRUDS
-
+    /**
+     * Get all transactions of a specific user
+     *
+     * @param userId the id of a user
+     * @return ResponseEntity object containing a list of transactions
+     */
     @GetMapping("{id}/transactions")
     public ResponseEntity<List<TransactionDTO>> getAllUserTransactions(@PathVariable("id") int userId) {
         return ResponseEntity.ok(transactionService.getAllTransactionsByUserId(userId));
     }
 
+    /**
+     * Get all transactions of a specific type of a specific user
+     *
+     * @param userId          the id of a user
+     * @param transactionType the type of a transaction (ex. income)
+     * @return ResponseEntity object containing a list of transactions
+     */
     @GetMapping("{id}/transactions/type/{type}/all")
     public ResponseEntity<List<TransactionDTO>> getAllUserTransactionsByType(@PathVariable("id") int userId, @PathVariable("type") String transactionType) {
         return ResponseEntity.ok(transactionService.getAllUserTransactionsByType(userId, transactionType));
     }
 
+    /**
+     * Get all transactions of a specific occurrence of a specific user
+     *
+     * @param userId         the id of a user
+     * @param occurrenceType the occurrence type of a transactions (ex. monthly)
+     * @return ResponseEntity object containing a list of transactions
+     */
     @GetMapping("{id}/transactions/occurrence/{occurrence}/all")
     public ResponseEntity<List<TransactionDTO>> getAllUserTransactionsByOccurrence(@PathVariable("id") int userId, @PathVariable("occurrence") String occurrenceType) {
         return ResponseEntity.ok(transactionService.getAllUserTransactionsByOccurrence(userId, occurrenceType));
     }
 
+    /**
+     * Get all transactions of a specific category of a specific user
+     *
+     * @param userId     the id of a user
+     * @param categoryId the id of a category
+     * @return ResponseEntity object containing a list of transactions
+     */
     @GetMapping("{userId}/transactions/category/{categoryId}/all")
     public ResponseEntity<List<TransactionDTO>> getAllUserTransactionsByCategory(@PathVariable("userId") int userId, @PathVariable("categoryId") int categoryId) {
         return ResponseEntity.ok(transactionService.getAllUserTransactionsByCategory(userId, categoryId));
