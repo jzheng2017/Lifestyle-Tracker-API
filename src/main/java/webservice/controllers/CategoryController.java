75e3f55eadee6b1fe8f40d4ba378fc766baec982
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-28 15:42:59 +0100

    add: javadoc to category controller

--- src/main/java/webservice/controllers/CategoryController.java
+++ src/main/java/webservice/controllers/CategoryController.java
@@ -20,17 +20,41 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    /**
+     * Get all categories
+     * <br>
+     * If the order or order by field is not specified, it will use the default value
+     *
+     * @param order   the order in which the list should be ordered (ex. ascending)
+     * @param orderBy the field that should be ordered on
+     * @return ResponseEntity object containing a list of categories
+     */
     @GetMapping("/all")
     public ResponseEntity<List<CategoryDTO>> getAllCategories(@RequestParam(value = "order", defaultValue = "asc") String order, @RequestParam(value = "orderBy", defaultValue = "id") String orderBy) {
         return ResponseEntity.ok(categoryService.getAll(order, orderBy));
     }
 
-
+    /**
+     * Get a single category
+     *
+     * @param categoryId the id of a category
+     * @return ResponseEntity object containing a category object
+     */
     @GetMapping("{id}")
     public ResponseEntity<CategoryDTO> getCategory(@PathVariable("id") int categoryId) {
         return ResponseEntity.ok(categoryService.getCategory(categoryId));
     }
 
+    /**
+     * Get all children of a category
+     * <br>
+     * If the order or order by field is not specified, it will use the default value
+     *
+     * @param parentId the id of a parent category
+     * @param order    the order in which the list should be ordered (ex. ascending)
+     * @param orderBy  the field that should be ordered on
+     * @return ResponseEntity object containing a list of categories
+     */
     @GetMapping("{id}/children")
     public ResponseEntity<List<CategoryDTO>> getAllChildren(@PathVariable("id") int parentId,
                                                             @RequestParam(value = "order", defaultValue = "asc") String order,
@@ -38,16 +62,34 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getChildren(parentId, order, orderBy));
     }
 
+    /**
+     * Create a category
+     *
+     * @param categoryDTO the category request object
+     * @return ResponseEntity object containing the added category
+     */
     @PostMapping("/create")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         return ResponseEntity.ok(categoryService.createCategory(categoryDTO));
     }
 
+    /**
+     * Update a category
+     *
+     * @param categoryDTO the category containing the new values
+     * @return ResponseEntity object containing the updated category
+     */
     @PutMapping("/update")
     public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         return ResponseEntity.ok(categoryService.updateCategory(categoryDTO));
     }
 
+    /**
+     * Delete a category
+     *
+     * @param categoryId the id of a category
+     * @return ResponseEntity object containing a boolean value whether the category has been successfully deleted
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> deleteCategory(@PathVariable("id") int categoryId) {
         return ResponseEntity.ok(categoryService.deleteCategory(categoryId));
