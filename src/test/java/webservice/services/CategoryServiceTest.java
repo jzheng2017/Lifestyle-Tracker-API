62cfd5e0a19b02f25ca77d9425b389d932329f1d
Jiankai Zheng <jk.zheng@hotmail.com>
2020-01-10 18:33:15 +0100

    change: updated unit tests to new service changes

--- src/test/java/webservice/services/CategoryServiceTest.java
+++ src/test/java/webservice/services/CategoryServiceTest.java
@@ -1,5 +1,6 @@
 package webservice.services;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
@@ -8,13 +9,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import webservice.dto.CategoryDTO;
 import webservice.entities.Category;
+import webservice.entities.QCategory;
 import webservice.exceptions.ResourceNotFoundException;
 import webservice.repositories.CategoryRepository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,45 +36,55 @@ public class CategoryServiceTest {
     private CategoryDTO mockedCategoryDTO;
     @Mock
     private Category mockedCategory;
+    @Mock
+    private Predicate predicate;
+    @Mock
+    private Pageable pageable;
+    @Mock
+    private Page<Category> pageList;
 
-    private final List<Category> list = new ArrayList<>();
+    private List<Category> list;
+
+    private Optional<Category> category;
+
+    private Optional<Category> emptyCategory;
+
+
     private final int parentId = 1;
     private final int categoryId = 1;
-    private final String orderAsc = "asc";
-    private final String orderDesc = "desc";
-    private final String orderBy = "id";
-    private final Sort.Direction sortDirectionAsc = Sort.Direction.ASC;
-    private final Sort.Direction sortDirectionDesc = Sort.Direction.DESC;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        category = Optional.of(mockedCategory);
+        emptyCategory = Optional.empty();
+        list = new ArrayList<>(Arrays.asList(mockedCategory));
+        when(modelMapper.map(any(), any())).thenReturn(mockedCategoryDTO);
+        when(categoryRepository.findById(categoryId)).thenReturn(category);
+        when(pageList.getContent()).thenReturn(list);
     }
 
     @Test
     public void getAllReturnsListOfCategories() {
-        when(categoryRepository.findAll(Sort.by(sortDirectionAsc, orderBy))).thenReturn(list);
+        when(categoryRepository.findAll(predicate, pageable)).thenReturn(pageList);
 
-        Assertions.assertNotNull(categoryService.getAll(orderAsc, orderBy));
+        Assertions.assertNotNull(categoryService.getAll(predicate, pageable));
     }
 
     @Test
-    public void getAllBySortDirectionCallsCategoryRepositoryFindAllFunctionWithCorrectSortDirection() {
-        categoryService.getAll(orderAsc, orderBy);
-        categoryService.getAll(orderDesc, orderBy);
+    public void getAllByPredicateAndPageablePassesCorrectArgumentToRepositoryFunction() {
+        when(categoryRepository.findAll(predicate, pageable)).thenReturn(pageList);
 
-        verify(categoryRepository).findAll(Sort.by(sortDirectionAsc, orderBy)); //categoryService.getAll(orderAsc, orderBy);
-        verify(categoryRepository).findAll(Sort.by(sortDirectionDesc, orderBy)); //categoryService.getAll(orderDesc, orderBy);
+        categoryService.getAll(predicate, pageable);
+
+        verify(categoryRepository).findAll(predicate, pageable);
     }
 
     @Test
     public void getAllMapsTheReturnedValues() {
-        when(categoryRepository.findAll(Sort.by(sortDirectionAsc, orderBy))).thenReturn(list);
-        when(modelMapper.map(any(), any())).thenReturn(mockedCategoryDTO);
+        when(categoryRepository.findAll(predicate, pageable)).thenReturn(pageList);
 
-        list.add(mockedCategory); //add at least one category so it goes inside the map function
-
-        categoryService.getAll(orderAsc, orderBy);
+        categoryService.getAll(predicate, pageable);
 
         verify(modelMapper, atLeastOnce()).map(any(), any());
         verify(modelMapper, atMostOnce()).map(any(), any()); //list (in this test) contains only 1 category so it should be called at least 1 time and at most 1 time
@@ -78,20 +92,11 @@ public class CategoryServiceTest {
 
     @Test
     public void getCategoryReturnsCategory() {
-        final Optional<Category> category = Optional.of(mockedCategory);
-
-        when(modelMapper.map(any(), any())).thenReturn(mockedCategoryDTO);
-        when(categoryRepository.findById(categoryId)).thenReturn(category);
-
         Assertions.assertEquals(mockedCategoryDTO, categoryService.getCategory(categoryId));
     }
 
     @Test
     public void getCategoryMapsValue() {
-        final Optional<Category> category = Optional.of(mockedCategory);
-
-        when(categoryRepository.findById(categoryId)).thenReturn(category);
-
         categoryService.getCategory(categoryId);
 
         verify(modelMapper).map(any(), any());
@@ -99,20 +104,17 @@ public class CategoryServiceTest {
 
     @Test
     public void getCategoryThrowsResourceNotFoundWhenNoCategoryReturned() {
-        final Optional<Category> category = Optional.empty();
-
-        when(categoryRepository.findById(categoryId)).thenReturn(category);
+        when(categoryRepository.findById(categoryId)).thenReturn(emptyCategory);
 
         Assertions.assertThrows(ResourceNotFoundException.class, () -> categoryService.getCategory(categoryId));
     }
 
     @Test
     public void getCategoryReturnsCorrectExceptionMessage() {
-        final Optional<Category> category = Optional.empty();
         final String expectedMessage = "Category not found";
         ResourceNotFoundException exception;
 
-        when(categoryRepository.findById(categoryId)).thenReturn(category);
+        when(categoryRepository.findById(categoryId)).thenReturn(emptyCategory);
 
         exception = Assertions.assertThrows(ResourceNotFoundException.class, () -> categoryService.getCategory(categoryId));
 
@@ -121,35 +123,38 @@ public class CategoryServiceTest {
 
     @Test
     public void getChildrenReturnsListOfCategories() {
-        when(categoryRepository.findAllByParentId(parentId, Sort.by(sortDirectionAsc, orderBy))).thenReturn(list);
+        final Predicate expectedPredicate = QCategory.category.parent.id.eq(parentId).and(predicate);
 
-        Assertions.assertNotNull(categoryService.getChildren(parentId, orderAsc, orderBy));
+        when(categoryRepository.findAll(expectedPredicate, pageable)).thenReturn(pageList);
+
+        Assertions.assertNotNull(categoryService.getChildren(parentId, predicate, pageable));
     }
 
     @Test
-    public void getChildrenBySortDirectionsCallsCategoryRepositoryFindAllByParentIdWithCorrectSortDirection() {
-        categoryService.getChildren(parentId, orderAsc, orderBy);
-        categoryService.getChildren(parentId, orderDesc, orderBy);
+    public void getChildrenByPredicateAndPageablePassesCorrectArgumentToRepositoryFunction() {
+        final Predicate expectedPredicate = QCategory.category.parent.id.eq(parentId).and(predicate);
 
-        verify(categoryRepository).findAllByParentId(parentId, Sort.by(sortDirectionAsc, orderBy)); //categoryService.getChildren(parentId, orderAsc, orderBy);
-        verify(categoryRepository).findAllByParentId(parentId, Sort.by(sortDirectionDesc, orderBy)); //categoryService.getChildren(parentId, orderDesc, orderBy);
+        when(categoryRepository.findAll(expectedPredicate, pageable)).thenReturn(pageList);
+
+        categoryService.getChildren(parentId, predicate, pageable);
+
+        verify(categoryRepository).findAll(expectedPredicate, pageable);
     }
 
     @Test
     public void getChildrenMapsTheReturnedValues() {
-        when(categoryRepository.findAllByParentId(parentId, Sort.by(sortDirectionAsc, orderBy))).thenReturn(list);
-        when(modelMapper.map(any(), any())).thenReturn(mockedCategoryDTO);
+        final Predicate expectedPredicate = QCategory.category.parent.id.eq(parentId).and(predicate);
 
-        list.add(mockedCategory); //add at least one category so it goes inside the map function
+        when(categoryRepository.findAll(expectedPredicate, pageable)).thenReturn(pageList);
 
-        categoryService.getChildren(parentId, orderAsc, orderBy);
+        categoryService.getChildren(parentId, predicate, pageable);
 
         verify(modelMapper, atLeastOnce()).map(any(), any());
         verify(modelMapper, atMostOnce()).map(any(), any()); //list (in this test) contains only 1 category so it should be called at least 1 time and at most 1 time
     }
 
     @Test
-    public void createCategoryCallsCreateUpdateFunction(){
+    public void createCategoryCallsCreateUpdateFunction() {
 //        categoryService.createCategory(mockedCategoryDTO);
 //        verify(categoryService).c
     }
