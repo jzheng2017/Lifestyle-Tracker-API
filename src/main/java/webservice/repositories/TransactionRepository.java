f14f889a4cf4eca516331c634fed37ff4363cb87
Jiankai Zheng <jk.zheng@hotmail.com>
2020-01-20 00:02:23 +0100

    remove: removed unused functions
    removed unnecessary functions after adding querydsl support

--- src/main/java/webservice/repositories/TransactionRepository.java
+++ src/main/java/webservice/repositories/TransactionRepository.java
@@ -6,21 +6,5 @@ import webservice.entities.Transaction;
 
 import java.util.List;
 
-public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
-
-    List<Transaction> findAllByUserId(int userId);
-
-    List<Transaction> findAllByTransactionType_Type(Sort by, String type);
-
-    List<Transaction> findAllByOccurrence_Name(Sort by, String name);
-
-    List<Transaction> findAllByCategory_Id(Sort by, int categoryId);
-
-    List<Transaction> findAllByUserIdAndTransactionType_Type(int userId, String type);
-
-    List<Transaction> findAllByUserIdAndOccurrence_Name(int userId, String occurence);
-
-    List<Transaction> findAllByUserIdAndCategoryId(int userId, int categoryId);
-
-    List<Transaction> findAll(Sort by);
+public interface TransactionRepository extends BaseRepository<Transaction, Integer> {
 }
