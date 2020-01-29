971056ad4021732b3337d78c8f8ca1d79c1276cb
jzheng2017 <jk.zheng@hotmail.com>
2020-01-29 21:46:17 +0100

    add: added collection mapping function

--- src/main/java/webservice/util/mappers/UserMapper.java
+++ src/main/java/webservice/util/mappers/UserMapper.java
@@ -5,11 +5,21 @@ import webservice.dto.RegistrationDTO;
 import webservice.dto.UserDTO;
 import webservice.entities.User;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserMapper extends ClassMapper {
 
     public UserDTO mapToUserDTO(User user) {
         return modelMapper.map(user, UserDTO.class);
+    }
+
+    public List<UserDTO> mapToUserDTOList(List<User> userList) {
+        return userList
+                .stream()
+                .map(entity -> modelMapper.map(entity, UserDTO.class))
+                .collect(Collectors.toList());
     }
 
     public User mapToUser(UserDTO user) {
